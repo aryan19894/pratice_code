a15774817f34f9e06db4be64aec6fbc674f2b4fd
googne19894 <aryanraj19894@gmail.com>
2022-01-12 04:28:19 +0530

    pratice

--- src/main/java/com/striver/DSAExperience/Day2Array/CountInversionsInAnArray_Hard.java
+++ src/main/java/com/striver/DSAExperience/Day2Array/CountInversionsInAnArray_Hard.java
@@ -7,17 +7,14 @@ package com.striver.DSAExperience.Day2Array;
 
 import java.io.PrintStream;
 
-public class CountInversionsInAnArray_Hard
-{
+public class CountInversionsInAnArray_Hard {
 
-    static int getInvCount1(int arr[])
-    {
+    static int getInvCount1(int arr[]) {
         int n = arr.length;
         int count = 0;
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = i + 1; j < n; j++)
-                if(arr[i] > arr[j])
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++)
+                if (arr[i] > arr[j])
                     count++;
 
         }
@@ -25,17 +22,14 @@ public class CountInversionsInAnArray_Hard
         return count;
     }
 
-    static int getInvCount2(int arr[])
-    {
+    static int getInvCount2(int arr[]) {
         int n = arr.length;
         return mergeSort(arr, new int[n], 0, n - 1);
     }
 
-    private static int mergeSort(int arr[], int temp[], int left, int right)
-    {
+    private static int mergeSort(int arr[], int temp[], int left, int right) {
         int count = 0;
-        if(right > left)
-        {
+        if (right > left) {
             int mid = (left + right) / 2;
             count += mergeSort(arr, temp, left, mid);
             count += mergeSort(arr, temp, mid + 1, right);
@@ -44,35 +38,31 @@ public class CountInversionsInAnArray_Hard
         return count;
     }
 
-    private static int mergeSortedArray(int arr[], int temp[], int left, int mid, int right)
-    {
+    private static int mergeSortedArray(int arr[], int temp[], int left, int mid, int right) {
         int count = 0;
         int i = left;
         int j = mid;
         int k = left;
-        while(i <= mid - 1 && j <= right) 
-            if(arr[i] <= arr[j])
-            {
+        while (i <= mid - 1 && j <= right)
+            if (arr[i] <= arr[j]) {
                 temp[k++] = arr[i++];
-            } else
-            {
+            } else {
                 temp[k++] = arr[j++];
                 count += mid - i;
             }
-        while(i <= mid - 1) 
+        while (i <= mid - 1)
             temp[k++] = arr[i++];
-        while(j <= right) 
+        while (j <= right)
             temp[k++] = arr[j++];
-        for(i = left; i <= right; i++)
+        for (i = left; i <= right; i++)
             arr[i] = temp[i];
 
         return count;
     }
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         int arr[] = {
-            5, 3, 2, 4, 1
+                5, 3, 2, 4, 1
         };
         int ans = getInvCount2(arr);
         System.out.println(ans);
