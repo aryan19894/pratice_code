3a64d85acab7d5477143540ff2784e110af68b82
googne19894 <aryanraj19894@gmail.com>
2022-01-11 08:52:55 +0530

    merging all branch to master

---
+++ src/main/java/com/striver/DSAExperience/Day2Array/RotateImage_Medium_48.java
@@ -0,0 +1,50 @@
+package com.striver.DSAExperience.Day2Array;
+
+import com.common.Printer;
+
+public class RotateImage_Medium_48
+{
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[][] nums = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        sol.rotate(nums);
+        Printer.print(nums);
+    }
+
+    static class Solution {
+        Solution() {
+        }
+
+        public void rotate(int[][] matrix) {
+            int i;
+            int j;
+            int k;
+            for(i = 0; i < matrix.length; ++i) {
+                for(j = i; j < matrix[0].length; ++j) {
+                    k = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = k;
+                }
+            }
+
+            for(i = 0; i < matrix.length; ++i) {
+                j = 0;
+                k = matrix[i].length - 1;
+
+                while(j < k) {
+                    swap(matrix[i], j++, k--);
+                }
+            }
+
+        }
+
+        private static void swap(int[] arr, int a, int b) {
+            if (a != b) {
+                int t = arr[a];
+                arr[a] = arr[b];
+                arr[b] = t;
+            }
+
+        }
+    }
+}
---
+++ src/main/java/com/striver/DSAExperience/Day2Array/SetMatrixZeroes_73_Medium.java
@@ -0,0 +1,115 @@
+// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+// Source File Name:   SetMatrixZeroes_73_Medium.java
+
+package com.striver.DSAExperience.Day2Array;
+
+import com.common.Obj;
+import com.common.Printer;
+
+import java.util.*;
+
+public class SetMatrixZeroes_73_Medium {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[][] nums = new int[][]{{1, 2, 3, 4}, {5, 0, 7, 8}, {0, 10, 11, 12}, {13, 14, 15, 0}};
+        sol.setZeroes3(nums);
+        Printer.print(nums);
+    }
+
+    static class Solution {
+        Solution() {
+        }
+
+        public void setZeroes1(int[][] matrix) {
+            int row = matrix.length;
+            int col = matrix[0].length;
+            List<int[]> zeroes = new ArrayList();
+
+            for (int i = 0; i < row; ++i) {
+                for (int j = 0; j < col; ++j) {
+                    if (matrix[i][j] == 0) {
+                        zeroes.add(new int[]{i, j});
+                    }
+                }
+            }
+
+            Iterator var8 = zeroes.iterator();
+
+            while (var8.hasNext()) {
+                int[] z = (int[]) var8.next();
+
+                int c;
+                for (c = 0; c < col; ++c) {
+                    matrix[z[0]][c] = 0;
+                }
+
+                for (c = 0; c < row; ++c) {
+                    matrix[c][z[1]] = 0;
+                }
+            }
+
+        }
+
+        public void setZeroes2(int[][] matrix) {
+            int r = matrix.length;
+            int c = matrix[0].length;
+            int[] row = new int[r];
+            int[] col = new int[c];
+
+            int i;
+            int j;
+            for (i = 0; i < r; ++i) {
+                for (j = 0; j < c; ++j) {
+                    if (matrix[i][j] == 0) {
+                        row[i] = -1;
+                        col[j] = -1;
+                    }
+                }
+            }
+
+            for (i = 0; i < matrix.length; ++i) {
+                for (j = 0; j < matrix[0].length; ++j) {
+                    if (row[i] == -1 || col[j] == -1) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+
+        }
+
+        public void setZeroes3(int[][] matrix) {
+            int r = matrix.length;
+            int c = matrix[0].length;
+            boolean col0 = true;
+
+            int i;
+            int j;
+            for (i = 0; i < r; ++i) {
+                if (matrix[i][0] == 0) {
+                    col0 = false;
+                }
+
+                for (j = 1; j < c; ++j) {
+                    if (matrix[i][j] == 0) {
+                        matrix[i][0] = matrix[0][j] = 0;
+                    }
+                }
+            }
+
+            for (i = r - 1; i >= 0; --i) {
+                for (j = c - 1; j >= 1; --j) {
+                    if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
+
+                if (!col0) {
+                    matrix[i][0] = 0;
+                }
+            }
+
+        }
+    }
+}
