396f71ef8cbd02258b1331e59ce52c887a506217
googne19894 <aryanraj19894@gmail.com>
2022-01-12 07:14:31 +0530

    pratice

---
+++ src/main/java/com/striver/DSAExperience/Day4Hashing/LargestSubArrayWith0Sum_Easy.java
@@ -0,0 +1,16 @@
+package com.striver.DSAExperience.Day4Hashing;
+
+public class LargestSubArrayWith0Sum_Easy {
+    static class Solution {
+        int maxLen(int arr[], int n) {
+            return 0;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {1, 2, 0, 1, 3, 3};
+        System.out.println(sol.maxLen(nums, nums.length));
+    }
+}
