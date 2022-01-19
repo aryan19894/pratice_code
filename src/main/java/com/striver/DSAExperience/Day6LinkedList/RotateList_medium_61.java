3d39a14fd8cd065e82aa03078da3e8f2484dca5c
googne19894 <aryanraj19894@gmail.com>
2022-01-19 10:10:52 +0530

    Pratice 16-0-2022 reversal of file

---
+++ src/main/java/com/striver/DSAExperience/Day6LinkedList/RotateList_medium_61.java
@@ -0,0 +1,37 @@
+package com.striver.DSAExperience.Day6LinkedList;
+
+import com.striver.DSAExperience.Day5LinkedList.LinkedList;
+import com.striver.DSAExperience.Day5LinkedList.ListNode;
+
+public class RotateList_medium_61 {
+    static class Solution {
+        public ListNode rotateRight(ListNode head, int k) {
+            if (head == null || head.next == null || k == 0) return head;
+
+            int len = 1;
+            ListNode dummy = head;
+            while (dummy.next != null) {
+                len++;
+                dummy = dummy.next;
+            }
+            dummy.next = head;
+            k %= len;
+            k = len - k;
+            while (k-- > 0)
+                dummy = dummy.next;
+
+            head = dummy.next;
+            dummy.next = null;
+
+            return head;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        LinkedList l1 = new LinkedList(new int[]{1, 2, 3, 4, 5});
+
+        ListNode res = sol.rotateRight(l1.head, 3);
+        res.print();
+    }
+}
