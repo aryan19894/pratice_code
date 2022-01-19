a2a4c33e17c6db0de94e340a40ed458cc7fe98b7
googne19894 <aryanraj19894@gmail.com>
2022-01-19 12:10:16 +0530

    Pratice 19-01-2022

---
+++ src/main/java/com/striver/DSAExperience/Day7TwoPointer/Sum3_medium_15.java
@@ -0,0 +1,29 @@
+package com.striver.DSAExperience.Day7TwoPointer;/*
+ * @lc app=leetcode id=15 lang=java
+ *
+ * [15] 3Sum
+ */
+
+import com.common.Obj;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// @lc code=start
+public class Sum3_medium_15 {
+    static class Solution {
+        public List<List<Integer>> threeSum(int[] nums) {
+            List<List<Integer>> res = new ArrayList<>();
+            return res;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(sol.threeSum(nums));
+    }
+}
+
+// @lc code=end
+
