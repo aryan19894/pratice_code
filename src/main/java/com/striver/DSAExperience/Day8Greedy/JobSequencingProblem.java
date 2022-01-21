4d47f57f96312c5a9c240f507bcc2d615e533fed
googne19894 <aryanraj19894@gmail.com>
2022-01-21 23:49:05 +0530

    Pratice 20,21-01-2022

---
+++ src/main/java/com/striver/DSAExperience/Day8Greedy/JobSequencingProblem.java
@@ -0,0 +1,62 @@
+package com.striver.DSAExperience.Day8Greedy;
+
+import com.common.Printer;
+import jdk.nashorn.internal.scripts.JO;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class JobSequencingProblem {
+    static int[] JobScheduling(Job[] arr, int n) {
+        Arrays.sort(arr, (a, b) -> (b.profit - a.profit));
+        int[] res = new int[2];
+        int maxDeadline = Integer.MIN_VALUE;
+        for (Job j : arr)
+            maxDeadline = Math.max(maxDeadline, j.deadline);
+        int[] jobSchedule = new int[maxDeadline + 1];
+        Arrays.fill(jobSchedule, -1);
+
+        for (Job j : arr) {
+            if (res[0] == maxDeadline)
+                break;
+
+            int currentElement = j.deadline;
+            while (currentElement > 0) {
+                if (jobSchedule[currentElement--] == -1) {
+                    jobSchedule[++currentElement] = j.id;
+                    res[0]++;
+                    res[1] += j.profit;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int nums[][] = {{1, 4, 20}, {2, 5, 60}, {3, 6, 70}, {4, 6, 65}
+                , {5, 4, 25}, {6, 2, 80}, {7, 2, 10}, {8, 2, 22}};
+        int[] ans = JobScheduling(Job.toJob(nums), nums.length);
+        Printer.print(ans);
+    }
+}
+
+class Job {
+    int id, profit, deadline;
+
+    public Job(int id, int deadline, int profit) {
+        this.id = id;
+        this.profit = profit;
+        this.deadline = deadline;
+    }
+
+    public static Job[] toJob(int[][] nums) {
+        Job[] jobs = new Job[nums.length];
+        int i = 0;
+        for (int[] n : nums) {
+            jobs[i++] = new Job(n[0], n[1], n[2]);
+        }
+        return jobs;
+    }
+}
