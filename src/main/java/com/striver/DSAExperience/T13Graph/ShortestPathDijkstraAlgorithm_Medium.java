7c7aa5c7892eb2e80e5870bc56f7cf040d7d9cb4
googne19894 <aryanraj19894@gmail.com>
2022-02-16 09:30:41 +0530

    Graph Pratice 16-02-2022

---
+++ src/main/java/com/striver/DSAExperience/T13Graph/ShortestPathDijkstraAlgorithm_Medium.java
@@ -0,0 +1,41 @@
+package com.striver.DSAExperience.T13Graph;
+
+import com.common.Out;
+import com.common.converter.Array;
+import com.striver.DSAExperience.T13Graph.common.Edge;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class ShortestPathDijkstraAlgorithm_Medium {
+    // [Optimal] TC: O((N+E)*logN). Going through N nodes and E edges and log N for priority queue
+    // SC:  O(N). Distance array and priority queue
+    static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S) {
+        int[] distance = new int[V];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[S] = 0;
+        PriorityQueue<Edge> pq = new PriorityQueue<>(V, new Edge());
+
+        pq.offer(new Edge(S, 0));
+        while (!pq.isEmpty()) {
+            Edge node = pq.poll();
+            for (ArrayList<Integer> child : adj.get(node.v())) {
+                if (distance[node.v()] + child.get(1) < distance[child.get(0)]) {
+                    distance[child.get(0)] = distance[node.v()] + child.get(1);
+                    pq.offer(new Edge(child.get(0), distance[child.get(0)]));
+                }
+            }
+        }
+        return distance;
+    }
+
+    public static void main(String[] args) {
+        int[][][] graph = {{{1, 1}, {2, 7}, {4, 9}},
+                {{0, 1}, {3, 6}, {4, 3}},
+                {{0, 7}, {4, 9}},
+                {{1, 6}},
+                {{0, 9}, {1, 3}, {2, 9}}};
+        Out.print(dijkstra(5, Array.toList(graph), 1));
+    }
+}
---
+++ src/main/java/com/striver/DSAExperience/T13Graph/common/Edge.java
@@ -0,0 +1,89 @@
+package com.striver.DSAExperience.T13Graph.common;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Edge implements Comparator<Edge> {
+    private int u;
+    private int v;
+    private int weight;
+
+    public Edge(int u, int v, int weight) {
+        this.u = u;
+        this.v = v;
+        this.weight = weight;
+    }
+
+    public Edge(int _v, int _weight) {
+        v = _v;
+        weight = _weight;
+    }
+
+    public Edge() {
+    }
+
+    public int u() {
+        return u;
+    }
+
+    public int v() {
+        return v;
+    }
+
+    public int weight() {
+        return weight;
+    }
+
+    @Override
+    public int compare(Edge o1, Edge o2) {
+        return o1.weight - o2.weight;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Edge e = (Edge) o;
+        if (this.u == e.u && this.v == e.v && this.weight == e.weight)
+            return true;
+        if (this.u == e.v && this.v == e.u && this.weight == e.weight)
+            return true;
+        if (this == o)
+            return true;
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "u=" + u +
+                ", v=" + v +
+                ", weight=" + weight +
+                '}';
+    }
+
+    public static ArrayList<Edge> convert(ArrayList<ArrayList<ArrayList<Integer>>> graph) {
+        Set<Edge> edges = new HashSet<>();
+        int i = 0;
+        for (ArrayList<ArrayList<Integer>> g : graph) {
+            for (ArrayList<Integer> gs : g) {
+                edges.add(new Edge(i, gs.get(0), gs.get(1)));
+            }
+            i++;
+        }
+        return new ArrayList<Edge>(edges);
+    }
+
+    public static int[] toArray(int V, ArrayList<Edge> edges) {
+        int[] graph = new int[V];
+        for(Edge e: edges){
+            graph[e.v] = e.u;
+        }
+        return graph;
+    }
+}
