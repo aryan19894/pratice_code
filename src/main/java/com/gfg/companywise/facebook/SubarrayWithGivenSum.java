44ea03b4e218158ce9579a0d2b651e9259cc6448
googne19894 <aryanraj19894@gmail.com>
2022-02-20 14:59:18 +0530

    gfg facebook start

---
+++ src/main/java/com/gfg/companywise/facebook/SubarrayWithGivenSum.java
@@ -0,0 +1,20 @@
+package com.gfg.companywise.facebook;
+
+import com.common.Out;
+
+import java.util.*;
+
+public class SubarrayWithGivenSum {
+	static ArrayList<Integer> subarraySum(int[] arr, int n, int s) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {};
+		int n = 0;
+		int s = 0;
+		Out.print(subarraySum(arr, n, s));
+	}
+}
