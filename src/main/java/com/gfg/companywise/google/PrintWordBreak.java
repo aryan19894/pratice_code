323a19d2ba6362ccf6fbc7d70562327b9077d121
googne19894 <aryanraj19894@gmail.com>
2022-03-23 13:11:23 +0530

    GFG Google Finish 22, 23-Mar-2022

--- src/main/java/com/gfg/companywise/google/PrintWordBreak.java
+++ src/main/java/com/gfg/companywise/google/PrintWordBreak.java
@@ -1,74 +1,32 @@
 package com.gfg.companywise.google;
 
-import com.common.Out;
 import com.common.converter.Array;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-public class WordBreak {
-    public static int wordBreak(String A, ArrayList<String> B) {
+public class PrintWordBreak {
+    public static void wordBreak(String A, ArrayList<String> B) {
         Set<String> dict = new HashSet<>();
         for (String d : B)
             dict.add(d);
 
-        return check(A, dict) ? 1 : 0;
+        printWordBreak(A.length(), A, dict, "");
     }
 
-    private static boolean check(String str, Set<String> dict) {
-        int n = str.length();
-        if (n == 0) return true;
+    private static void printWordBreak(int n, String str, Set<String> dict, String ans) {
         for (int i = 1; i <= n; i++) {
-            if (dict.contains(str.substring(0, i)) && check(str.substring(i, n), dict))
-                return true;
-        }
-        return false;
-    }
-
-    public static int wordBreak2(String A, ArrayList<String> B) {
-        int n = A.length();
-        if (n == 0) return 1;
-
-        Set<String> dict = new HashSet<>();
-        for (String d : B)
-            dict.add(d);
-
-        boolean[] dp = new boolean[n + 1];
-        for (int i = 1; i <= n; i++) {
-            if (dict.contains(A.substring(0, i))) dp[i] = true;
-            if (dp[i]) {
-                if (i == n) return 1;
-                for (int j = i + 1; j <= n; j++) {
-                    if (!dp[j] && dict.contains(A.substring(i, j))) dp[j] = true;
-                    if (j == n && dp[j]) return 1;
+            String prefix = str.substring(0, i);
+            if (dict.contains(prefix)) {
+                if (i == n) {
+                    ans += prefix;
+                    System.out.println(ans);
+                    return;
                 }
+                printWordBreak(n - i, str.substring(i, n), dict, ans + prefix + " ");
             }
         }
-
-        return 0;
-    }
-
-    public static int wordBreak3(String A, ArrayList<String> B) {
-        int n = A.length();
-        if (n == 0) return 1;
-
-        Set<String> dict = new HashSet<>();
-        for (String d : B)
-            dict.add(d);
-
-        boolean[] dp = new boolean[n + 1];
-        dp[0] = true;
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && dict.contains(A.substring(j, i))) {
-                    dp[i] = true;
-                    break;
-                }
-            }
-        }
-
-        return dp[n] ? 1 : 0;
     }
 
     public static void main(String[] args) {
@@ -77,8 +35,6 @@ public class WordBreak {
                 "man", "mango", "icecream", "and",
                 "go", "i", "like", "ice", "cream"};
         ArrayList<String> B = Array.toList(dict);
-        Out.print(wordBreak(A, B));
-        Out.print(wordBreak2(A, B));
-        Out.print(wordBreak3(A, B));
+        wordBreak(A, B);
     }
 }
--- src/main/java/revise/striver/DSAExperience/T1Array/SortColors_medium_75.java
+++ src/main/java/revise/striver/DSAExperience/T1Array/SortColors_medium_75.java
@@ -1,14 +1,37 @@
-/*
- * @lc app=leetcode id=75 lang=java
- *
- * [75] Sort Colors
- */
+package revise.striver.DSAExperience.T1Array;
 
-// @lc code=start
-class Solution {
-    public void sortColors(int[] nums) {
-        
+public class SortColors_medium_75 {
+    static class Solution {
+        // [Better]: find the frequency array and assign the value w.r.t its frequency
+        public void sortColors(int[] nums) {
+            int[] freq = new int[3];
+            for (int num : nums)
+                freq[num]++;
+
+            int id = 0;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < freq[i]; j++)
+                    nums[id++] = i;
+            }
+        }
+
+        // [Better]: find the frequency array and assign the value w.r.t its frequency
+        public void sortColors2(int[] nums) {
+            int[] freq = new int[3];
+            for (int num : nums)
+                freq[num]++;
+
+            int id = 0;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < freq[i]; j++)
+                    nums[id++] = i;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        sol.sortColors(nums);
     }
 }
-// @lc code=end
-
