83764756251916c8c758dafd20f4f75bb5d9e0b9
googne19894 <aryanraj19894@gmail.com>
2022-03-31 11:34:47 +0530

    Leetcode Blind75 Binary Finish 31-Mar-2022

--- src/main/java/com/leetcode/blind75/T2Binary/NumberOf1Bits_easy_191.java
+++ src/main/java/com/leetcode/blind75/T2Binary/NumberOf1Bits_easy_191.java
@@ -2,19 +2,21 @@ package com.leetcode.blind75.T2Binary;
 
 import com.common.Out;
 
-import java.util.*;
-
 public class NumberOf1Bits_easy_191 {
-	static public class Solution {
-	    public int hammingWeight(int n) {
-			int result = 0;
+    static public class Solution {
+        public int hammingWeight(int n) {
+            int count = 0;
+            while (n != 0) {
+                count += n & 1;
+                n >>>= 1; // unsigned right shift
+            }
+            return count;
+        }
+    }
 
-			return result;
-	    }
-	}
-	public static void main(String[] args) {
-		Solution sol = new Solution();
-		int n = 0;
-		Out.print(sol.hammingWeight(n));
-	}
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int n = 00000000000000000000000000001011;
+        Out.print(sol.hammingWeight(n));
+    }
 }
