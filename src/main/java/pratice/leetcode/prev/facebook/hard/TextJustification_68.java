70bb3aa1f3b63172c84d3af3fabaf3a4e5c61068
googne19894 <aryanraj19894@gmail.com>
2022-05-12 14:43:17 +0530

    Previous FB Question Finish 12-May

--- src/main/java/pratice/leetcode/prev/facebook/hard/TextJustification_68.java
+++ src/main/java/pratice/leetcode/prev/facebook/hard/TextJustification_68.java
@@ -1,7 +1,5 @@
 package pratice.leetcode.prev.facebook.hard;
 
-import pratice.common.Out;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,35 +7,63 @@ public class TextJustification_68 {
     static class Solution {
         public List<String> fullJustify(String[] words, int maxWidth) {
             List<String> result = new ArrayList<>();
-            List<String> sub = new ArrayList<>();
-            int currLen = 0;
-            for (String word : words) {
-//                if (currLen + word.length() <= maxWidth) {
-//                    currLen += word.length() + 1;
-//                    sub.add(word);
-//                }
-                if(currLen + word.length() > maxWidth) {
-                    StringBuffer sb = new StringBuffer();
-                    for (int i = 0; i < sub.size() - 1; i++)
-                        sb.append(sub.get(i) + " ");
-                    for (int i = currLen; i < maxWidth; i++)
-                        sb.append(" ");
-                    sb.append(sub.get(sub.size() - 1));
-                    result.add(new String(sb));
-                    currLen = 0;
-                    sub = new ArrayList<>();
+            int i = 0, n = words.length;
+            while (i < n) {
+                StringBuilder sb = new StringBuilder();
+                List<String> line = new ArrayList<>();
+                int currLen = 0;
+                boolean thisLine = true;
+
+                while (i < n && thisLine) {
+                    String word = words[i];
+                    if (currLen + line.size() + word.length() <= maxWidth) {
+                        line.add(word);
+                        currLen += word.length();
+                        i++;
+                    } else thisLine = false;
                 }
-                currLen += word.length() + 1;
-                sub.add(word);
+
+                int spaces = maxWidth - currLen;
+                if (line.size() == 1 || i == n) {
+                    for (String w : line) {
+                        sb.append(w);
+                        if (spaces-- > 0) {
+                            sb.append(' ');
+                        }
+                    }
+                    appendSpace(sb, spaces);
+                } else {
+                    int lineSize = line.size();
+                    int commonSpace = spaces / (lineSize - 1);
+                    int extraSpace = spaces % (lineSize - 1);
+                    for (int j = 0; j < lineSize; j++) {
+                        String word = line.get(j);
+                        sb.append(word);
+                        if (j == lineSize - 1) continue;
+                        appendSpace(sb, commonSpace);
+                        if (extraSpace > 0) {
+                            sb.append(' ');
+                            extraSpace--;
+                        }
+                    }
+                }
+                result.add(sb.toString());
             }
             return result;
+        }
+
+        private void appendSpace(StringBuilder sb, int spaces) {
+            for (int j = 0; j < spaces; j++)
+                sb.append(' ');
         }
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        String[] words = {"What", "must", "be", "acknowledgment", "shall", "be"};
+        String[] words = {"What","must","be","acknowledgment","shall","be"};
         int maxWidth = 16;
-        Out.print(sol.fullJustify(words, maxWidth));
+        List<String> result = sol.fullJustify(words, maxWidth);
+        for (String r : result)
+            System.out.println("\""+r+"\"");
     }
 }
