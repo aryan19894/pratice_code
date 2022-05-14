25fd2961742fad0b5a2d690c5052c3c33b9e1664
googne19894 <aryanraj19894@gmail.com>
2022-05-14 14:07:01 +0530

    Leetcode Blind75 String Finish 14-May-2022

--- src/main/java/pratice/leetcode/blind75/T8String/LongestPalindromicSubstring_medium_5.java
+++ src/main/java/pratice/leetcode/blind75/T8String/LongestPalindromicSubstring_medium_5.java
@@ -7,48 +7,64 @@ public class LongestPalindromicSubstring_medium_5 {
         public String longestPalindrome(String s) {
             int n = s.length();
             boolean[][] dp = new boolean[n][n];
-            int maxLength = 0, startIdx = 0;
+            int maxLength = 1;
+            int start = 0;
 
-            for (int i = 0; i < n; i++) {
-                for (int j = i; j >= 0; j--) {
-                    boolean isMatch = s.charAt(j) == s.charAt(i);
-                    if (j == i) dp[j][i] = true;
-                    else if (j == i - 1) dp[j][i] = isMatch;
-                    else dp[j][i] = isMatch && dp[j + 1][i - 1];
+            for (int i = 0; i < n; i++) dp[i][i] = true;
+            for (int i = 0; i < n - 1; i++) {
+                if (s.charAt(i) == s.charAt(i + 1)) {
+                    dp[i][i + 1] = true;
+                    start = i;
+                    maxLength = 2;
+                }
+            }
 
-                    int currLen = i - j + 1;
-                    if (dp[j][i] && currLen > maxLength) {
-                        maxLength = currLen;
-                        startIdx = j;
+            for (int currLen = 3; currLen <= n; currLen++) {
+                for (int i = 0; i < n - currLen + 1; i++) {
+                    int j = i + currLen - 1;
+
+                    if (dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = true;
+                        if (maxLength < currLen) {
+                            maxLength = currLen;
+                            start = i;
+                        }
                     }
                 }
             }
-            return s.substring(startIdx, startIdx + maxLength);
+            return s.substring(start, start + maxLength);
         }
 
         public String longestPalindrome2(String s) {
-            StringBuffer sb = new StringBuffer();
-            lcs(s, s.length() - 1, 0, sb, 0);
-            return new String(sb);
+            int start = 0, end = 0;
+            for (int i = 0; i < s.length(); i++) {
+                int oddSubLen = expand(s, i, i);
+                int evenSubLen = expand(s, i, i + 1);
+                int len = Math.max(oddSubLen, evenSubLen);
+
+                int currLen = end - start + 1;
+                if (len > currLen) {
+                    start = i - (len - 1) / 2;
+                    end = i + len / 2;
+                }
+            }
+            return s.substring(start, end + 1);
         }
 
-        private int lcs(String s, int i, int j, StringBuffer sb, int count) {
-            if (i == 0 || j == s.length()) return count;
-
-            if (s.charAt(i) == s.charAt(j))
-                count = lcs(s, i - 1, j + 1, sb.append(s.charAt(i)), count + 1);
-            else {
-//                lcs(s, i - 1, j, sb);
-//                lcs(s, i, j + 1, sb);
+        private int expand(String s, int l, int r) {
+            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                l--;
+                r++;
             }
-            return count;
+            return r - l - 1;
         }
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
         String s = "babad";
-//        Out.print(sol.longestPalindrome(s));
+        Out.print(sol.longestPalindrome(s));
         Out.print(sol.longestPalindrome2(s));
+        Out.print(sol.longestPalindrome(s));
     }
 }
