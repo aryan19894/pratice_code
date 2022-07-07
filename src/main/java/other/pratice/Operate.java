374fac25420728c9bc943520228ea2257349f69c
googne19894 <aryanraj19894@gmail.com>
2022-07-07 17:33:40 +0530

    Switch Stage2: Finish

---
+++ src/main/java/other/pratice/Operate.java
@@ -0,0 +1,9 @@
+package other.pratice;
+
+public class Operate {
+    public static void main(String[] args) {
+        short a = 1, b = 2;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
