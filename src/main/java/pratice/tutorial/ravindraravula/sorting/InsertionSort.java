e5df41701aef32bb312c2de01f2f59578faef16f
googne19894 <aryanraj19894@gmail.com>
2022-07-22 14:57:45 +0530

    Ravindrababu: Sorting 8-Jul

--- src/main/java/pratice/tutorial/ravindraravula/sorting/InsertionSort.java
+++ src/main/java/pratice/tutorial/ravindraravula/sorting/InsertionSort.java
@@ -8,8 +8,26 @@ public class InsertionSort {
         System.out.println("Before Sorting: ");
         Out.print(a);
         insertionSort(a, a.length);
-        System.out.println("After Sorting: ");
+        System.out.println("After Sorting Iterative: ");
         Out.print(a);
+
+        a = new int[]{9, 6, 5, 0, 8, 2, 7, 1, 3};
+        System.out.println("Before Sorting: ");
+        Out.print(a);
+        insertionSort(a, 1, a.length);
+        System.out.println("After Sorting Recursive: ");
+        Out.print(a);
+    }
+
+    private static void insertionSort(int[] a, int i, int n) {
+        if (i == n) return;
+
+        int pick = a[i];
+        int j = i - 1;
+        while (j >= 0 && a[j] > pick)
+            a[j + 1] = a[j--];
+        a[j + 1] = pick;
+        insertionSort(a, i + 1, n);
     }
 
     private static void insertionSort(int[] a, int n) {
@@ -18,7 +36,7 @@ public class InsertionSort {
             int j = i - 1;
             while (j >= 0 && a[j] > pick)
                 a[j + 1] = a[j--];
-            a[j + 1] =pick;
+            a[j + 1] = pick;
         }
     }
 }
