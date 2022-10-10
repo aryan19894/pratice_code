d188317964497e8032fd80b527fb9e5ec900fde7
googne19894 <aryanraj19894@gmail.com>
2022-10-10 09:17:55 +0530

    Striver: Revise: LinkedList 8 Oct

---
+++ src/main/java/revise/striver/DSAExperience/T3LinkedList/DeleteNodeInALinkedList_easy_237.java
@@ -0,0 +1,17 @@
+package revise.striver.DSAExperience.T3LinkedList;
+
+public class DeleteNodeInALinkedList_easy_237 {
+    static class Solution {
+        public void deleteNode(ListNode node) {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        ListNode head = new ListNode(4, 5, 1, 9);
+        sol.deleteNode(head.next);
+        head.print();
+    }
+}
